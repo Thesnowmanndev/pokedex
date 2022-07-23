d903382016ab9e5ceae3a679ff61c3ff9905e970
Kyle Martin <kyle.martin.0819@gmail.com>
2022-07-23 16:33:12 -0400

    Pokedex created
    Added a pokedex page for each generation. Pokedex pages require styling still. Pokedex pages load images, index number, and name.

---
+++ pages/alola.js
@@ -0,0 +1,62 @@
+import Layout from "../components/Layout.js";
+import Link from "next/link";
+import Image from "next/image";
+
+export default function alola({ pokemon }) {
+  return (
+    <Layout title="Alola Pokedex">
+      <div className="flex justify-center hover:cursor-none">
+        <h1 className="mb-8 text-4xl uppercase">
+          <span className="border-b-2 border-sky-500 hover:text-orange-500 hover:cursor-none">
+            Alola Region | Pokedex
+          </span>
+        </h1>
+      </div>
+      <div className="flex justify-center">
+        <ul className="grid items-center grid-cols-1 md:grid-cols-2 xl:grid-cols-3">
+          {pokemon.map((eachPokemon, index) => (
+            <li key={index} className="mx-1">
+              <Link href={`/pokemon?id=${index + 722}`}>
+                <a>
+                  <div className="my-4 shadow-xl w-96 card card-side bg-base-100">
+                    <figure>
+                      <img src={eachPokemon.image} alt={eachPokemon.name}></img>
+                    </figure>
+                    <div className="card-body">
+                      <h2 className="capitalize card-title">
+                        <span>{index + 722}.</span> {eachPokemon.name}
+                      </h2>
+                    </div>
+                  </div>
+                </a>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </Layout>
+  );
+}
+
+export async function getStaticProps() {
+  try {
+    const res = await fetch(
+      "https://pokeapi.co/api/v2/pokemon?limit=88&offset=721"
+    );
+    const { results } = await res.json();
+    const pokemon = results.map((result, index) => {
+      const paddedIndex = ("00" + (index + 722)).slice(-3);
+      const image = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${paddedIndex}.png`;
+      return {
+        ...result,
+        image,
+      };
+    });
+
+    return {
+      props: { pokemon },
+    };
+  } catch (err) {
+    console.error("Alert: " + err);
+  }
+}
